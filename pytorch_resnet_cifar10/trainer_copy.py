84f5e6d41666efe3bea9d6f14c032b42e1ed721c
Rakshith Srinivasa <rakshith.sharma.s@gmail.com>
2018-04-09 12:17:13 -0400

    adding print checkpoint

--- pytorch_resnet_cifar10/trainer_copy.py
+++ pytorch_resnet_cifar10/trainer_copy.py
@@ -81,6 +81,7 @@ def main():
         if os.path.isfile(args.resume):
             print("=> loading checkpoint '{}'".format(args.resume))
             checkpoint = torch.load(args.resume)
+            print(list(checkpoint.keys()))
             args.start_epoch = checkpoint['epoch']
             best_prec1 = checkpoint['best_prec1']
             model.load_state_dict(checkpoint['state_dict'])
