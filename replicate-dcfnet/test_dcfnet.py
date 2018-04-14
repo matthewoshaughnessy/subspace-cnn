14fa2e8903d77779beb6a4fb23b4044595e2e233
Matt O'Shaughnessy <matthewoshaughnessy@gatech.edu>
2018-04-14 16:26:17 -0400

    debugging

--- replicate-dcfnet/test_dcfnet.py
+++ replicate-dcfnet/test_dcfnet.py
@@ -85,9 +85,12 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(512, nClasses)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)
         x = x.view(-1, 256 * 1 * 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
