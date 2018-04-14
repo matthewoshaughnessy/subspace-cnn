83be64fd490ee9fdfb0d00a8c377d5d3ed6ad745
Matt O'Shaughnessy <matthewoshaughnessy@gatech.edu>
2018-04-14 16:19:14 -0400

    debugging

--- replicate-dcfnet/test_dcfnet.py
+++ replicate-dcfnet/test_dcfnet.py
@@ -77,16 +77,18 @@ if torch.cuda.is_available():
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, (5,5))
-        self.conv2 = nn.Conv2d(16, 64, (5,5))
+        self.conv1 = nn.Conv2d(3, 64, (5,5))
+        self.conv2 = nn.Conv2d(64, 128, (5,5))
+        self.conv3 = nn.Conv3d(128, 256, (5,5))
         self.pool = nn.MaxPool2d(3, 3)
-        self.fc1 = nn.Linear(64, 128)
-        self.fc2 = nn.Linear(128, nClasses)
+        self.fc1 = nn.Linear(256, 512)
+        self.fc2 = nn.Linear(512, nClasses)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 64 * 1 * 1)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 256 * 1 * 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return x
