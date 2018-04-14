f1efccb0ea80367ca42e1a933097d381f944a248
Matt O'Shaughnessy <matthewoshaughnessy@gatech.edu>
2018-04-14 17:05:03 -0400

    debugging

--- replicate-dcfnet/test_dcfnet.py
+++ replicate-dcfnet/test_dcfnet.py
@@ -125,9 +125,9 @@ basis2 = scipy.fftpack.dct(np.eye(H2*W2),norm='ortho')
 ### Define a Loss function and optimizer ################################
 
 criterion = nn.CrossEntropyLoss()
-#optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-optimizer = torch.optim.Adam(net.parameters(), lr=1e-5)
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+#optimizer = torch.optim.Adam(net.parameters(), lr=1e-5)
+#scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
 
 #### Train the network #################################################
 
