95c68bfc565787a2e5eb7db52a6edbc3b4c6a3a1
Matt O'Shaughnessy <matthewoshaughnessy@gatech.edu>
2018-04-14 16:33:58 -0400

    debugging

--- replicate-dcfnet/test_dcfnet.py
+++ replicate-dcfnet/test_dcfnet.py
@@ -56,13 +56,13 @@ transform = transforms.Compose(
 # get training data
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=256,
                                           shuffle=True, num_workers=2)
 
 # get (potentially noisy) test data
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=64,
+testloader = torch.utils.data.DataLoader(testset, batch_size=256,
                                          shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
