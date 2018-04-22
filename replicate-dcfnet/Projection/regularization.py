3ee99f6ae04af888ac71d3434e05aaba8e194fc0
Matt O'Shaughnessy <matthewoshaughnessy@gatech.edu>
2018-04-22 11:58:10 -0400

    debugging subspace projection on gpu

--- replicate-dcfnet/Projection/regularization.py
+++ replicate-dcfnet/Projection/regularization.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.fftpack
+import torch
 
 
 def subspace_projection(k,w,basis=None, basis_indices=None):
@@ -29,3 +30,38 @@ def subspace_projection(k,w,basis=None, basis_indices=None):
             w_projected[ii,jj,:] = np.dot(B,w_reshaped[ii,jj,:])
     w_projected = np.reshape(w_projected,(F,C,HH,WW),'F')
     return w_projected
+
+def subspace_projection_gpu(k,w,basis=None, basis_indices=None):
+    """
+    n: Image height
+    k: subspace dimension
+    F: number of filters
+    w: Filter coefficient: (F x C x HH x WW )
+    """
+    F,C,HH,WW = w.shape
+    if basis is None:
+        basis = scipy.fftpack.dct(np.eye(HH*WW),norm='ortho')
+    
+    #w_reshaped = np.reshape(w,(F,C,HH*WW),'F')
+    w_reshaped = torch.view(w,(F,C,HH*WW)) # TODO -- 'F'?  transpose first?
+
+    #w_projected = np.zeros(w_reshaped.shape)
+    w_projected = torch.zeros(w_reshaped.shape)
+    
+    if basis_indices is None:
+        #basis_indices = np.zeros((F,k))
+        basis_indices = torch.zeros((F,k))
+        for ii in range(F):
+            basis_indices[ii,:] = np.arange(k)
+            
+    for ii in range(F):
+        indices = basis_indices[ii,:].astype(int)
+        for jj in range(C):
+            #B = np.dot(basis[:,indices],basis[:,indices].T)
+            B = torch.mm(basis[:,indices],basis[:,indices].T)
+            #w_projected[ii,jj,:] = np.dot(B,w_reshaped[ii,jj,:])
+            w_projected[ii,jj,:] = torch.mm(B,w_reshaped[ii,jj,:])
+    #w_projected = np.reshape(w_projected,(F,C,HH,WW),'F')
+    w_projected = torch.view(w_projected,(F,C,HH,WW))
+    return w_projected
+
--- replicate-dcfnet/dcfnet.py
+++ replicate-dcfnet/dcfnet.py
@@ -230,9 +230,9 @@ for epoch in range(nEpochs):  # loop over the dataset multiple times
                     w2 = net.conv2.weight.data
                     w3 = net.conv2.weight.data
                     print( type(w1) )
-                    w1p = (subspace_projection(net.dim1,w1,net.basis1,net.basis_indices1))
-                    w2p = (subspace_projection(net.dim2,w2,net.basis2,net.basis_indices2))
-                    w3p = (subspace_projection(net.dim3,w3,net.basis3,net.basis_indices3))
+                    w1p = (subspace_projection_gpu(net.dim1,w1,net.basis1,net.basis_indices1))
+                    w2p = (subspace_projection_gpu(net.dim2,w2,net.basis2,net.basis_indices2))
+                    w3p = (subspace_projection_gpu(net.dim3,w3,net.basis3,net.basis_indices3))
                     net.conv1.weight.data = (torch.from_numpy(w1p)).type(torch.FloatTensor)
                     net.conv2.weight.data = (torch.from_numpy(w2p)).type(torch.FloatTensor)
                     net.conv3.weight.data = (torch.from_numpy(w3p)).type(torch.FloatTensor)
