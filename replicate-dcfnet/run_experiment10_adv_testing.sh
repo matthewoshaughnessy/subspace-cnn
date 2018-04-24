ef2593b1425c61c01b3545de16eff476bcfe477d
Rakshith Srinivasa <rakshith.sharma.s@gmail.com>
2018-04-24 08:18:25 -0400

    adv testing

--- replicate-dcfnet/run_experiment10_adv_testing.sh
+++ replicate-dcfnet/run_experiment10_adv_testing.sh
@@ -1,5 +1,6 @@
+python3 dcfnet_adv.py out_adv_noproj_nonoise false false dct
+python3 dcfnet_adv.py out_adv_proj_nonoise true false dct
 python3 dcfnet_testing_only.py out_adv_test_noproj_001 './model_out_adv_noproj_nonoise' 0.001
-
 python3 dcfnet_testing_only.py out_adv_test_proj_001 './model_out_adv_proj_nonoise' 0.001
 python3 dcfnet_testing_only.py out_adv_test_noproj_005 './model_out_adv_noproj_nonoise' 0.005
 python3 dcfnet_testing_only.py out_adv_test_proj_005 './model_out_adv_proj_nonoise' 0.005
